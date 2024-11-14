ee8a9d34d98af7c5c32cc75e373046746b930fbd
matthias.druwe <matthias.druwe@odisee.be>
2024-11-14 11:41:32 +0100

    Add demo les 7 Hangman TDD

---
+++ Les7/Hangman/Hangman.Tests/HangmanGameTests.cs
@@ -0,0 +1,154 @@
+﻿using Hangman.Repositories;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Tests
+{
+    internal class HangmanGameTests
+    {
+
+        [Test]
+        public void ctor_WithWordsRepo_GetRandomWoordWordtAangeroepen()
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+          
+            // Act
+            HangmanGame sut = new HangmanGame(woordRepository);
+
+            // Assert
+            woordRepository.Received(1).GetRandomWoord();
+        }
+
+        [TestCase("Appel", "_____")]
+        [TestCase("Peer", "____")]
+        [TestCase("Banaan", "______")]
+        public void ctor_MetRandomWoord_ReturnsJuisteAantalLiggendeStreepjes(string randomwoord, string verwachtResultaat)
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            woordRepository.GetRandomWoord().Returns(randomwoord);
+
+            // Act
+            HangmanGame sut = new HangmanGame(woordRepository);
+
+            // Assert
+            Assert.That(sut.TeRadenWoord, Is.EqualTo(verwachtResultaat));
+        }
+
+        [Test]
+        public void RaadLetter_MetReedsgekozenLetter_ReturnsLetterAlgeraden()
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            HangmanGame sut = new HangmanGame(woordRepository);
+            sut.RaadLetter('a');
+
+            // Act
+            GameStatus result = sut.RaadLetter('a');
+
+            // Assert
+            Assert.That(result, Is.EqualTo(GameStatus.AlGeraden));
+        }
+
+        [Test]
+        public void RaadLetter_MetLetterAanwezigInWoord_ReturnsLetterAanwezigEnLevensBlijvenOngewijzigd()
+        {
+
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            woordRepository.GetRandomWoord().Returns("appel");
+            HangmanGame sut = new HangmanGame(woordRepository);
+
+            // Act
+            GameStatus result = sut.RaadLetter('a');
+
+            // Assert
+            Assert.That(result, Is.EqualTo(GameStatus.LetterAanwezig));
+            Assert.That(sut.Levens, Is.EqualTo(10));
+        }
+
+        [TestCase("appel", 'a', "a____")]
+        [TestCase("appel", 'p', "_pp__")]
+        [TestCase("peer", 'p', "p___")]
+        public void RaadLetter_MetLetterAanwezigInWoord_ReplaceUnderscoreWithGuessedLetter(string teRadenWoord, char letter, string result)
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            woordRepository.GetRandomWoord().Returns(teRadenWoord);
+            HangmanGame sut = new HangmanGame(woordRepository);
+
+            // Act
+            sut.RaadLetter(letter);
+
+            // Assert
+            Assert.That(sut.TeRadenWoord, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void RaadLetter_MetAlCorrectGeradenLetters_TeRadenWoordHoudtRekeningMetEerderGeradenLetters()
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            woordRepository.GetRandomWoord().Returns("appel");
+            HangmanGame sut = new HangmanGame(woordRepository);
+            sut.RaadLetter('a');
+
+            // act
+            sut.RaadLetter('p');
+
+            // Assert
+            Assert.That(sut.TeRadenWoord, Is.EqualTo("app__"));
+        }
+
+        [Test]
+        public void RaadLetter_MetLetterNietAanwezigInWoord_ReturnsLetterAfwezig()
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            woordRepository.GetRandomWoord().Returns("peer");
+            HangmanGame sut = new HangmanGame(woordRepository);
+
+            // Act
+            GameStatus result = sut.RaadLetter('a');
+
+            // Assert
+            Assert.That(result, Is.EqualTo(GameStatus.LetterAfwezig));
+        }
+
+        [Test]
+        public void RaadLetter_MetLetterNietAanwezigInWoord_VerminderdLevensVanSpeler()
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            woordRepository.GetRandomWoord().Returns("appel");
+            HangmanGame sut = new HangmanGame(woordRepository);
+
+            // Act
+            sut.RaadLetter('z');
+
+            // Assert
+            Assert.That(sut.Levens, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void RaadLetter_MetLetterNietAanwezigInWoordEnAlFoutGeradenLettersAanwezig_VerminderdLevensVanSpeler()
+        {
+            // Arrange
+            IWoordRepository woordRepository = Substitute.For<IWoordRepository>();
+            woordRepository.GetRandomWoord().Returns("appel");
+            HangmanGame sut = new HangmanGame(woordRepository);
+
+            // Act
+            sut.RaadLetter('z');
+            sut.RaadLetter('y');
+
+            // Assert
+            Assert.That(sut.Levens, Is.EqualTo(8));
+        }
+    }
+}
---
+++ Les7/Hangman/Hangman/HangmanGame.cs
@@ -0,0 +1,63 @@
+﻿using Hangman.Repositories;
+
+namespace Hangman
+{
+    public class HangmanGame
+    {
+        public string TeRadenWoord {
+            get
+            {
+                return new string(
+                    randomWoord.Select(letter => letters.Contains(letter) ? letter: '_').ToArray());
+            }
+        }
+        public int Levens { get; private set; } = 10;
+
+        private string randomWoord;
+        private List<char> letters = new List<char>();
+
+        public HangmanGame(IWoordRepository woordRepository)
+        {
+            randomWoord = woordRepository.GetRandomWoord();
+    
+            //foreach(char c in randomWoord)
+            //{
+            //    TeRadenWoord += "_";
+            //}
+        }
+
+        public GameStatus RaadLetter(char c)
+        {
+            //int index = 0;
+            //foreach(char letter in randomWoord)
+            //{
+            //    if(letter == c)
+            //    {
+            //        char[] temp = TeRadenWoord.ToCharArray();
+            //        temp[index] = c;
+            //        TeRadenWoord = new string(temp);
+            //    } 
+            //    index++;
+
+            //}
+
+          
+
+            if (letters.Contains(c))
+            {
+                return GameStatus.AlGeraden;
+            }
+
+            letters.Add(c);
+
+            if (randomWoord.Contains(c))
+            {
+                return GameStatus.LetterAanwezig;
+            } else
+            {
+                Levens -= 1;
+                return GameStatus.LetterAfwezig;
+            }
+        }
+    }
+}
